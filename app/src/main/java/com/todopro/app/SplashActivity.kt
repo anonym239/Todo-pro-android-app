72fa28cf9c0dd665ff6f2b8c036ec33dad7ee4d6
anonym239 <todopro@github.com>
2026-04-26 10:29:58 +0200

    Add: Splash Screen, new App Icon (red checkmark), SplashActivity

---
+++ app/src/main/java/com/todopro/app/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.todopro.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Nach 1.5 Sekunden zur MainActivity wechseln
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1500)
+    }
+}
